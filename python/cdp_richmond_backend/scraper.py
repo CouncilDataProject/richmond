9c34276fd38b4cdba6544f1e069dff700e49360e
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-08-24 00:49:29 -0700

    Better scraper defaults and ignore patterns

--- python/cdp_richmond_backend/scraper.py
+++ python/cdp_richmond_backend/scraper.py
@@ -18,6 +18,17 @@ def get_events(
     scraper = LegistarScraper(
         client="richmondva",
         timezone="America/New_York",
+        ignore_minutes_item_patterns=[
+            "Citizens were encouraged to provide their comments in writing",
+            "This meeting will be held in-person",
+            "Special Guidelines for Public Access and Citizen",
+            "To access or participate, or both",
+            "The Honorable",
+            "Absent",
+            "A copy of the material provided has been filed",
+            "CERTIFICATION OF CLOSED MEETING",
+            "the Council has convened in closed meeting on this date",
+        ],
     )
 
     return scraper.get_events(begin=from_dt, end=to_dt)
